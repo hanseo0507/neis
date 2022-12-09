7c2e59ac462ed03b18285f0f510a8b5eedd169f6
HanSu Lee <i.hansu.lee@gmail.com>
2022-12-10 03:22:14 +0900

    test: add `removeLoadDTM` helper function and split mockup data to neis.mock.ts

--- test/neis.test.ts
+++ test/neis.test.ts
@@ -1,5 +1,7 @@
 import { Neis } from '../src';
-import { NeisSchoolInfo, Optional } from '../src/types';
+
+import { classInfoMock, schoolInfoMock, schoolMajorInfoMock } from './__mocks__/neis.mock';
+import { removeLoadDTM } from './helper/removeLoadDTM';
 
 describe('Check environment variables', () => {
   test('process.env.NEIS_API_KEY', () => {
@@ -18,44 +20,6 @@ describe('Define Neis() class', () => {
     expect(neis.key).toBe(process.env.NEIS_API_KEY || '');
   });
 
-  test('.getSchoolInfo()', async () => {
-    const schoolInfo = (await neis.getSchoolInfo({
-      SCHUL_NM: '한세사이버보안고등학교',
-    })) as Optional<NeisSchoolInfo, 'LOAD_DTM'>[];
-
-    schoolInfo[0].LOAD_DTM = undefined;
-
-    const expectValue = {
-      ATPT_OFCDC_SC_CODE: 'B10',
-      ATPT_OFCDC_SC_NM: '서울특별시교육청',
-      SD_SCHUL_CODE: '7010911',
-      SCHUL_NM: '한세사이버보안고등학교',
-      ENG_SCHUL_NM: 'Hansei Cyber Security High School',
-      SCHUL_KND_SC_NM: '고등학교',
-      LCTN_SC_NM: '서울특별시',
-      JU_ORG_NM: '서울특별시교육청',
-      FOND_SC_NM: '사립',
-      ORG_RDNZC: '04129 ',
-      ORG_RDNMA: '서울특별시 마포구 마포대로11길 44-80',
-      ORG_RDNDA: '(아현동)',
-      ORG_TELNO: '02-362-0130',
-      HMPG_ADRES: 'http://www.hansei.org',
-      COEDU_SC_NM: '남여공학',
-      ORG_FAXNO: '02-312-3996',
-      HS_SC_NM: '특성화고',
-      INDST_SPECL_CCCCL_EXST_YN: 'N',
-      HS_GNRL_BUSNS_SC_NM: '전문계',
-      SPCLY_PURPS_HS_ORD_NM: null,
-      ENE_BFE_SEHF_SC_NM: '전기',
-      DGHT_SC_NM: '주간',
-      FOND_YMD: '19941005',
-      FOAS_MEMRD: '19941005',
-      LOAD_DTM: undefined,
-    };
-
-    expect(schoolInfo[0]).toStrictEqual(expectValue);
-  });
-
   test('.getClassInfo()', async () => {
     const classInfo = await neis.getClassInfo({
       ATPT_OFCDC_SC_CODE: 'B10',
@@ -65,43 +29,12 @@ describe('Define Neis() class', () => {
       DDDEP_NM: '해킹보안과',
     });
 
-    const expectValues = [
-      {
-        ATPT_OFCDC_SC_CODE: 'B10',
-        ATPT_OFCDC_SC_NM: '서울특별시교육청',
-        SD_SCHUL_CODE: '7010911',
-        SCHUL_NM: '한세사이버보안고등학교',
-        AY: '2022',
-        GRADE: '2',
-        DGHT_CRSE_SC_NM: '주간',
-        SCHUL_CRSE_SC_NM: '고등학교',
-        ORD_SC_NM: '공업계',
-        DDDEP_NM: '해킹보안과',
-        CLASS_NM: '1',
-        LOAD_DTM: undefined,
-      },
-      {
-        ATPT_OFCDC_SC_CODE: 'B10',
-        ATPT_OFCDC_SC_NM: '서울특별시교육청',
-        SD_SCHUL_CODE: '7010911',
-        SCHUL_NM: '한세사이버보안고등학교',
-        AY: '2022',
-        GRADE: '2',
-        DGHT_CRSE_SC_NM: '주간',
-        SCHUL_CRSE_SC_NM: '고등학교',
-        ORD_SC_NM: '공업계',
-        DDDEP_NM: '해킹보안과',
-        CLASS_NM: '2',
-        LOAD_DTM: undefined,
-      },
-    ];
+    expect(removeLoadDTM(classInfo)).toStrictEqual(classInfoMock);
+  });
 
-    expect(
-      classInfo.map((info) => ({
-        ...info,
-        LOAD_DTM: undefined,
-      }))
-    ).toStrictEqual(expectValues);
+  test('.getSchoolInfo()', async () => {
+    const schoolInfo = await neis.getSchoolInfo({ SCHUL_NM: '한세사이버보안고등학교' });
+    expect(removeLoadDTM(schoolInfo)).toStrictEqual(schoolInfoMock);
   });
 
   test('.getSchoolMajorInfo()', async () => {
@@ -109,21 +42,6 @@ describe('Define Neis() class', () => {
       ATPT_OFCDC_SC_CODE: 'B10',
       SD_SCHUL_CODE: '7010911',
     });
-
-    expect(
-      schoolMajorInfo.map((info) => ({
-        ...info,
-        LOAD_DTM: undefined,
-      }))
-    ).toContainEqual({
-      ATPT_OFCDC_SC_CODE: 'B10',
-      ATPT_OFCDC_SC_NM: '서울특별시교육청',
-      SD_SCHUL_CODE: '7010911',
-      SCHUL_NM: '한세사이버보안고등학교',
-      DGHT_CRSE_SC_NM: '주간',
-      ORD_SC_NM: '공업계',
-      DDDEP_NM: '해킹보안과',
-      LOAD_DTM: undefined,
-    });
+    expect(removeLoadDTM(schoolMajorInfo)).toContainEqual(schoolMajorInfoMock);
   });
 });
